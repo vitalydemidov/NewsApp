9549d86f1e8ae9aee495fa6ad8f56369cf17fa2c
Виталий Демидов <mr.vitalydemidov@gmail.com>
2017-03-28 03:33:52 +0300

    Setup NavigationDrawer

--- app/src/main/java/ru/vitalydemidov/newsapp/sources/SourcesActivity.java
+++ app/src/main/java/ru/vitalydemidov/newsapp/sources/SourcesActivity.java
@@ -1,10 +1,16 @@
 package ru.vitalydemidov.newsapp.sources;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.UiThread;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import ru.vitalydemidov.newsapp.R;
 import ru.vitalydemidov.newsapp.data.source.SourcesRepository;
@@ -17,9 +23,10 @@ import ru.vitalydemidov.newsapp.util.ActivityUtils;
  */
 
 @UiThread
-public class SourcesActivity extends AppCompatActivity {
+public class SourcesActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-    private SourcesPresenter mSourcesPresenter;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,6 +36,7 @@ public class SourcesActivity extends AppCompatActivity {
         // Set up Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setupNavigationDrawer(toolbar);
 
         // Set up Fragment (View)
         SourcesFragment sourcesFragment =
@@ -40,9 +48,44 @@ public class SourcesActivity extends AppCompatActivity {
         }
 
         // Create Presenter
-        SourcesPresenter sourcesPresenter =
-                new SourcesPresenter(SourcesRepository.getInstance(SourcesRemoteDataSource.getInstance(),
-                        SourcesLocalDataSource.getInstance()), sourcesFragment);
+        new SourcesPresenter(SourcesRepository.getInstance(SourcesRemoteDataSource.getInstance(),
+                SourcesLocalDataSource.getInstance()), sourcesFragment);
     }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void setupNavigationDrawer(@NonNull Toolbar toolbar) {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    //region NavigationView.OnNavigationItemSelectedListener interface implementation
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.navigation_view_category_all) {
+
+        } else if (id == R.id.navigation_view_category_business) {
+
+        }
+
+        // FIXME: 28/03/2017 handle click
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    //endregion NavigationView.OnNavigationItemSelectedListener interface implementation
 
 }
