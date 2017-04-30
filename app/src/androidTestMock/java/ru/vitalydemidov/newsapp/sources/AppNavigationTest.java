b0df0a425247d8c2adacdeb58106eb8ecb494030
Виталий Демидов <mr.vitalydemidov@gmail.com>
2017-04-30 15:58:06 +0300

    Refactoring in AppNavigationTest

--- app/src/androidTestMock/java/ru/vitalydemidov/newsapp/sources/AppNavigationTest.java
+++ app/src/androidTestMock/java/ru/vitalydemidov/newsapp/sources/AppNavigationTest.java
@@ -18,16 +18,12 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.allOf;
 import static ru.vitalydemidov.newsapp.articles.ArticlesActivity.EXTRA_SOURCE;
 import static ru.vitalydemidov.newsapp.data.source.remote.NewsRemoteDataSource.SOURCES;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class AppNavigationTest {
-
-    private static final String PACKAGE_NAME = "ru.vitalydemidov.newsapp.articles";
-
 
     @Rule
     public IntentsTestRule<SourcesActivity> mIntentsTestRule =
@@ -42,13 +38,7 @@ public class AppNavigationTest {
         onView(withId(R.id.sources_recycler_view))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(position, click()));
 
-        intended(allOf(
-//                    hasComponent(hasShortClassName(".ArticlesActivity")),
-//                    toPackage(PACKAGE_NAME),
-                    hasExtra(Matchers.equalTo(EXTRA_SOURCE), Matchers.equalTo(expectedSource))
-                )
-        );
-
+        intended(hasExtra(Matchers.equalTo(EXTRA_SOURCE), Matchers.equalTo(expectedSource)));
     }
 
 }
